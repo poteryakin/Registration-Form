a53b0ad9dfb9661e9ef85e2570c2a396df56e252
Ilya Poteryaev <poteryaev.ilya2001@gmail.com>
2024-12-04 21:05:38 +0300

    update valid input

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const confirm_pass = document.getElementById('conf_password');
 const pass = document.getElementById('password');
+const number_phone = document.getElementById('phone_number');
+const email = document.getElementById('email');
 const btn_create = document.getElementById('button_create');
 const second_block = document.getElementById('second-block');
 const new_div = document.createElement('div');
@@ -7,27 +9,11 @@ let isElement = false;
 
 btn_create.addEventListener("click",create_account);
 
+number_phone.addEventListener('input', () => {number_phone.value = number_phone.value.replace(/[^+\d]/g, '')})
+
 function create_account() {
-    if (!isElement) {
-        second_block.appendChild(new_div);
-        isElement = true;
-    }
-    if (pass.value == '') {
-        new_div.innerText = '*Enter password';
-        border_red([pass,confirm_pass]);
-        new_div.style.color = 'red';
-    } 
-    else if (pass.value != confirm_pass.value) {
-        new_div.innerText = '*Passwords don\'t match';
-        border_red([pass,confirm_pass]);
-        new_div.style.color = 'red';
-    }
-    else {
-        pass.style = '';
-        confirm_pass.style = '';
-        second_block.removeChild(new_div);
-        isElement = false;
-    }
+    (pass.value != confirm_pass.value) ? confirm_pass.setCustomValidity('Passwords don\'t match') :
+        confirm_pass.setCustomValidity('');
 }
 
 function border_red(array) {
